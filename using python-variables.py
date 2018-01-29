a32e23cd854ba168b20d564c7007565321dfe9ad
Lilyann Martin <117227@district.evit.com>
2018-01-29 10:03:19 -0700

    added variable file

---
+++ using python-variables.py
@@ -0,0 +1,18 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: C:\Users\117227\using python-variables.py =============
+name?Lily
+__
+_
+Traceback (most recent call last):
+  File "C:\Users\117227\using python-variables.py", line 5, in <module>
+    print ("_") + 'str'(name) + ("__")
+TypeError: 'str' object is not callable
+>>> 
+============= RESTART: C:\Users\117227\using python-variables.py =============
+name?Lily
+___________
+__Lily__
+___________
+>>> 
